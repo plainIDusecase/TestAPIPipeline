09f1bb2012a83f435dc8ccd3712c03af2429094a
plainIDusecase <161554653+plainIDusecase@users.noreply.github.com>
2024-03-18 11:24:18 +1100

    Update PRODflow.py

--- project_code/PRODflow.py
+++ project_code/PRODflow.py
@@ -20,12 +20,19 @@ env2Id = "c3192543-bee5-40ee-87eb-3fd95853f22f"
 authWs2Id = "43419dfc-16b3-4b0d-8d7a-13a936ac02e8"
 approval = True
 
-policyCode = import_to_stage()
-isComplete = check_completion(env3Id, BEARER_TOKEN, policyCode, authWs3Id)
-if isComplete is True and approval is True:
-    print("Successfully moved the policy to PROD")
-    print("Move policy to PROD folder and push to GitHub")
+#If STAGE passes approval, export from STAGE and move to PROD
+env3Id = "c3895c00-9e78-4990-9342-4f296222a0a2"
+authWs3Id = "3c8017f4-fe6b-46ff-aff0-05014de21acc"
+#Validate the policy that was moved to STAGE
+[passfail,completed]=check_pass(env2Id,BEARER_TOKEN,policyCode,authWs2Id)
 
-else:
-    print("Failed to shift policy to PROD. Raise Ticket")
+policyCode = import_to_stage()
+for policy in policyCode:
+    isComplete = check_completion(env3Id, BEARER_TOKEN, policyCode, authWs3Id)
+    if isComplete is True and approval is True:
+        print("Successfully moved the policy to PROD")
+        print("Move policy to PROD folder and push to GitHub")
+
+    else:
+        print("Failed to shift policy to PROD. Raise Ticket")
 
