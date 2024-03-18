51907715ab98f4e268551e8d0833647fefb7d44c
plainIDusecase <161554653+plainIDusecase@users.noreply.github.com>
2024-03-18 11:02:34 +1100

    Update STAGEflow.py

--- project_code/STAGEflow.py
+++ project_code/STAGEflow.py
@@ -30,12 +30,15 @@ movedPolicies = []
 for policyTuple in policies:
     try:
         policyId, policyCode, *extra_values = policyTuple
+        #Validate the policy exported from DEV
         [passfail, completed] = check_pass(env1Id, BEARER_TOKEN, policyCode, authWs1Id)
         if passfail == [] and completed is True and approval is True:
             correctCodes.append([policyId, policyCode])
+            #Import policy from DEV into STAGE
             isComplete = check_completion(env2Id, BEARER_TOKEN, policyCode, authWs2Id)
             if isComplete is True:
                 completePolicies.append([policyId, policyCode])
+                #Export policy from STAGE
                 policyCode = get_policy(env2Id, policyId, BEARER_TOKEN)
                 env3Id = "c3895c00-9e78-4990-9342-4f296222a0a2"
                 authWs3Id = "3c8017f4-fe6b-46ff-aff0-05014de21acc"
